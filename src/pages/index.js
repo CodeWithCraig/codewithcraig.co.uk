5ca2bee7525c0b53ca6622238e4154326b5f3825
Craig Harvie <craig.harvie@bbc.co.uk>
2018-09-06 15:50:02 +0100

    Add updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
+import Logo from '../images/logo.png';
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  	<div className='hero'>
+  		<img src={Logo} alt='' />
+  	</div>
   </div>
 )
 
