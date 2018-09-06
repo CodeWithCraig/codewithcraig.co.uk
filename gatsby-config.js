bde4f326238637c8842fb2debae4814f5f0226b9
Craig Harvie <craig.harvie@bbc.co.uk>
2018-09-06 21:19:09 +0100

    Add favicon stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,23 @@ module.exports = {
   siteMetadata: {
     title: 'Code With Craig',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
+  plugins: [
+  	'gatsby-plugin-react-helmet',
+  	'gatsby-plugin-sass',
+  	{
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Karla\:400,700`,
+          `Spectral\:400,700`
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/images/favicon.png"
+      }
+    }
+  ]
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,13 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Logo from '../images/logo.png';
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+const Header = () => (
+  <header className='header'>
+    <Link to="/">
+      <img src={Logo} alt='' />
+    </Link>
+  </header>
 )
 
 export default Header
