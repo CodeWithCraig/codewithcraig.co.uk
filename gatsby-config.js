680c957a990084c3c74bb2931a79e3f13e402e33
Craig Harvie <craig.harvie@bbc.co.uk>
2018-09-06 21:30:45 +0100

    Add default options

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,19 @@ module.exports = {
     {
       resolve: `gatsby-plugin-favicon`,
       options: {
-        logo: "./src/images/favicon.png"
+        logo: "./src/images/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
       }
     }
   ]
